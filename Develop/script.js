2e7c5b1d6040435b559f4c3dd7d5ab954ee1d336
Dilberovicka31 <dilberovicm36@gmail.com>
2020-11-11 10:17:50 -0800

    finished html

--- Develop/script.js
+++ Develop/script.js
@@ -1,2 +1,18 @@
 var d = new Date();
     document.getElementById("currentDay").innerHTML = d;
+
+    //var i = 9;
+    //Element ID = "hour" + i;
+    //FOR  Loop over the hours of the day I = 9 to i <= 17
+
+    //Color code them depends on the past, current, future
+
+    //Currently checking  9AM < the current hour
+    //9am is in the past
+    //Select matching element - $(  Element id ) 
+
+    // OPTION -A Select matching element - $( Element ID ).css ("background");
+    // OPTION -B Select matching element - $( Element ID ).addClass ("is-past");
+
+    //Save data in the local storage - addEventlistener click on a parent element to listen to save button event delegation 
+    //event propagation 
