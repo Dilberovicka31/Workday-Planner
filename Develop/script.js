f2a69030e2f8beedd45407ba2ffc4f098b3364dc
Dilberovicka31 <dilberovicm36@gmail.com>
2020-11-13 16:24:35 -0800

    added a function to save button

--- Develop/script.js
+++ Develop/script.js
@@ -1,41 +1,51 @@
 
 var now = moment();
-var today = $("#currentDay").text(now.format("MMMM Do YYYY, h:mm:ss a" ));
-var currentTime = now.format("HH");
+var today = $("#currentDay").text(moment().format("dddd, MMMM, Do " ));
+var currentTime = moment().format("HH");
+console.log(today);
 
+var timePlace = [9,10,11,12,13,14,15,16,17];
+var currHour = parseInt(now.format("H HH"));
+console.log(currHour);
 
 var description = ($(".description"));
 var times = (".hour");
 var row = (".time-block");
 
 
-for (var i = 9; i < 18; i++) {
-   if (row < today); {
+for (var i = 0; i < 9; i++) {
+   if (times < currHour); {
         description.addClass("past");
     }
     
-    if (row === today ); {
+    if (times == currHour ); {
         description.addClass("present");
     }
     
-    if (row > today); {
+    if (times > currHour); {
         description.addClass("future");
     }
 
 }
 
+//if current time === block time . hour
 
-var saveBtn = $(".saveBtn").on("click", function(e) {
+
+ $(".saveBtn").on("click", function(e) {
     e.preventDefault();
- //Save data in the local storage - addEventlistener click on a parent element to listen to save button event delegation 
-    //event propagation 
-
+    var dataHour = $(this).attr("data-hr");
+    var inputField = $("#" + dataHour ).val();
+    localStorage.setItem(dataHour, inputField);
 
 })
 
+function renderPlanner (){
+    for (var i=0; i <= currentTime; i++){
+        $("#" + timePlace[i]).val(localStorage.getItem(timePlace[i]));
+    }
+}
 
-
-
+renderPlanner();
 
 
 
