a99566c56a8e4eeaa833a03693692a7b617e3228
Dilberovicka31 <dilberovicm36@gmail.com>
2020-11-13 18:39:45 -0800

    fixed the conditionals

--- Develop/script.js
+++ Develop/script.js
@@ -1,31 +1,31 @@
 
 var now = moment();
-var today = $("#currentDay").text(moment().format("dddd, MMMM, Do " ));
+var today = $("#currentDay").text(moment().format("dddd, MMMM, Do" ));
 var currentTime = moment().format("HH");
 console.log(today);
 
 var timePlace = [9,10,11,12,13,14,15,16,17];
-var currHour = parseInt(now.format("H HH"));
-console.log(currHour);
+
+console.log(currentTime);
 
 var description = ($(".description"));
 var times = (".hour");
 var row = (".time-block");
 
 
-for (var i = 0; i < 9; i++) {
-   if (times < currHour); {
+for (var i = 9; i < 18; i++) {
+   if (times < currentTime); {
         description.addClass("past");
     }
     
-    if (times == currHour ); {
-        description.addClass("present");
-    }
+    let (times === currentTime) ; {
+        description.addClass("present");}
     
-    if (times > currHour); {
+    
+    let (times > currentTime); 
         description.addClass("future");
-    }
-
+    
+    
 }
 
 //if current time === block time . hour
